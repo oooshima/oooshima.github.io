b472219e10a16d7efd501db2d918d3595bcbfefb
oshimaharuna <ooooooshim115@gmail.com>
2019-07-23 16:39:48 +0900

    add fullscreen

--- javascript/index.js
+++ javascript/index.js
@@ -3,4 +3,27 @@ $(function(){
   function mySwipeHandler( evt ){
     alert( '#myimg swiped.' );
   }
+
+window.onload = function() {
+  target = 66;
+  if (target.webkitRequestFullscreen) {
+    target.webkitRequestFullscreen();
+  }
+  //FF10+
+  else if (target.mozRequestFullScreen) {
+    target.mozRequestFullScreen();
+  }
+  //IE11+
+  else if (target.msRequestFullscreen) {
+    target.msRequestFullscreen();
+  }
+  // HTML5 Fullscreen API仕様
+  else if (target.requestFullscreen) {
+    target.requestFullscreen();
+  }
+  // 未対応
+  else {
+    alert('ご利用のブラウザはフルスクリーン操作に対応していません');
+  }
+}
 });
